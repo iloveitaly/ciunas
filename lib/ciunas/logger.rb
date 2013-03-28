19b2feff9788796f0e61eea62e72ea82a1ca9135
Michael Bianco <iloveitaly@gmail.com>
2013-03-28 10:31:25 -0400

    Rewrote how the silencer works. Using higher logging level to silence routing errors

--- lib/ciunas/logger.rb
+++ lib/ciunas/logger.rb
@@ -2,24 +2,17 @@ module Ciunas
   class Logger < Rails::Rack::Logger
     def initialize(app, opts = {})
       @app = app
-      @opts = opts
-      @silenced = @opts.delete(:silenced) || []
+      @silenced = opts.delete(:silenced) || []
       super
     end
 
     def call(env)
-      if env['X-SILENCE-LOGGER'] || @silenced.any? {|m| m === env['PATH_INFO'] }
-        begin
-          # temporarily set the rails log level to error
-          tmp_log_level = ActiveSupport::BufferedLogger::Severity::ERROR
-          old_logger_level, Rails.logger.level = Rails.logger.level, tmp_log_level
-          @app.call(env)
-        ensure
-          Rails.logger.level = old_logger_level
-        end
-      else
-        super(env)
-      end
+      # FATAL + 1 ensures that 404 errors are not logged
+      old_logger_level = Rails.logger.level
+      Rails.logger.level = ::Logger::FATAL + 1 if env['X-SILENCE-LOGGER'] || @silenced.any? {|m| m === env['PATH_INFO'] }
+      super
+    ensure
+      Rails.logger.level = old_logger_level
     end
   end
 end
